bb8184ef0f423f8352f328543a0f84357acfe8eb
tomorin <tanaka.t.bj@gmail.com>
2023-03-22 17:57:15 +0900

    Modified heisenberg.py

--- heisenberg.py
+++ heisenberg.py
@@ -7,7 +7,7 @@ from get_data.get_moments import get_moments
 # https://qiita.com/takubb/items/a2e02fe61aad80f8f2f3
 
 num_atoms = 2
-moments_file = "MOMENTS"
+moments_file = "MOMENTS_small"
 energies, moments = get_moments(moments_file, num_atoms)
 
 
@@ -17,23 +17,22 @@ for pattern in moments:
     vec_a = pattern[0][3:6]
     vec_b = pattern[1][3:6]
     x = np.append(x, np.dot(vec_a, vec_b))
-print(x)
 
 energies = energies*27.2114*1000/2
 
 
 with pm.Model() as model:
-    jij = pm.Normal('jij', mu=200, sigma=100)
+    jij = pm.Normal('jij', mu=0, sigma=1000)
     # noise = pm.Normal('noise', mu=0, sigma=1)
     noise = pm.HalfFlat('noise')
     b = pm.Normal('b', mu=0, sigma=np.std(energies))
-    y_pred = pm.Normal('y_pred', mu=-jij*x+b, sigma=noise, observed=energies)
+    y_pred = pm.Normal('y_pred', mu=-jij*(1-x)+b, sigma=noise, observed=energies)
     trace = pm.sample(draws=5000, chains=4) 
 
 #trace_n = trace[1000:]
 pm.plot_trace(trace)
 pm.summary(trace)
-plt.show()
+plt.figure()
 
 
 x_deg = np.degrees(np.arccos(x))
@@ -45,5 +44,7 @@ print(jij_m)
 print(b_m)
 print(np.std(energies))
 plt.plot(x_deg, energies, '.')
-plt.plot(x_deg_sort, -jij_m*np.cos(np.radians(x_deg_sort))+b_m, 'red', label='Estimated')
+x_mesh = np.linspace(0, 180, 180)
+plt.plot(x_mesh, -jij_m*(1-np.cos(np.radians(x_mesh)))+b_m, 'red', label='Estimated')
+plt.figure()
 plt.show()
